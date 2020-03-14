f44cfc95c1bb0000044d6aedc7f409349f0f0370
Soorena1991 <30846182+Soorena1991@users.noreply.github.com>
2020-03-14 19:48:13 +0100

    Create Dec.go

---
+++ Dec.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/sha3"
+)
+
+func help() {
+	fmt.Println("dec input_file_address key_file_address output_file_address")
+}
+
+func hash(data []byte) []byte {
+	hashArray := sha3.Sum256(data)
+	return hashArray[:]
+}
+
+func decrypt(data, pass []byte) []byte {
+	aesBlock, _ := aes.NewCipher(hash(pass))
+	gcm, err := cipher.NewGCM(aesBlock)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	nonce, cipherText := data[:gcm.NonceSize()], data[gcm.NonceSize():]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plainText
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		if len(os.Args) == 1 {
+			help()
+		} else {
+			fmt.Println("commandoo ride")
+		}
+		os.Exit(1)
+	}
+	//Open the file containing the string to be decrypted
+	inputFile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+	//Read input from the input file
+	inpuFileInfo, err := inputFile.Stat()
+	inputData := make([]byte, inpuFileInfo.Size())
+	_, err = inputFile.Read(inputData)
+	if err != nil {
+		panic(err.Error())
+	}
+	inputFile.Close()
+	//Open the key file
+	keyFile, err := os.Open(os.Args[2])
+	if err != nil {
+		panic(err.Error())
+	}
+	//Read key from key file
+	inpuFileInfo, err = keyFile.Stat()
+	key := make([]byte, inpuFileInfo.Size())
+	_, err = keyFile.Read(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyFile.Close()
+	//Create output file
+	outputFile, err := os.Create(os.Args[3])
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = outputFile.Write(decrypt(inputData, key))
+	if err != nil {
+		panic(err.Error())
+	}
+	outputFile.Close()
+}
